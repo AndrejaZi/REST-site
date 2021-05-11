413aaa785c12e0ed8a56f44fbb15c0b7ccda3046
AndrejaZi <andrejaziv1997@gmail.com>
2021-05-11 22:13:55 +0200

    removed from app.js

--- script/app.js
+++ script/app.js
@@ -1,30 +1,4 @@
 async function myFunc(){
 
-    //Place for REST API calls
-
-    // const baseUrl = "http://zutiws.dis.rs/serpod/SerPodM.dll";
-    // let request = "onl_lismpo";
-    // const url = baseUrl + "/" + request;
-    // console.log(url);
-
-    // const options = {
-    //     mode : "no-cors",
-    //     method : "POST",
-    //     body : {}
-    // };
-
-    // try {
-    //     // const result = await fetch(url, options);
-    //     // const data = result.json();
-    //     // console.log(data);
-
-    //     fetch(url, options)
-    //     .then(response => response.json())
-    //     .then(function (data) {
-    //       console.log(data);
-    //     });
-    // } catch (error) {
-    //     console.log("There has been an error" + error);
-    // }
    
 }
