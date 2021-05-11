84cc51616df904b25537968b598d6a04f6f21092
AndrejaZi <andrejaziv1997@gmail.com>
2021-05-11 20:45:52 +0200

    finished first look

--- script/app.js
+++ script/app.js
@@ -1,1 +1,30 @@
-alert("Hi there bro");
+async function myFunc(){
+
+    //Place for REST API calls
+
+    // const baseUrl = "http://zutiws.dis.rs/serpod/SerPodM.dll";
+    // let request = "onl_lismpo";
+    // const url = baseUrl + "/" + request;
+    // console.log(url);
+
+    // const options = {
+    //     mode : "no-cors",
+    //     method : "POST",
+    //     body : {}
+    // };
+
+    // try {
+    //     // const result = await fetch(url, options);
+    //     // const data = result.json();
+    //     // console.log(data);
+
+    //     fetch(url, options)
+    //     .then(response => response.json())
+    //     .then(function (data) {
+    //       console.log(data);
+    //     });
+    // } catch (error) {
+    //     console.log("There has been an error" + error);
+    // }
+   
+}
