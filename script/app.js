d1eb3a87d35d973d46cc6b963c7dcd2e8fb8a10e
AndrejaZi <andrejaziv1997@gmail.com>
2021-05-12 22:46:48 +0200

    app.js modifications

--- script/app.js
+++ script/app.js
@@ -1,4 +1,6 @@
-async function myFunc(){
 
-   
+async function myFunc(){
+    fetch('https://api.mocki.io/v1/ddb6d39b', { method: 'POST', mode : "no-cors" })
+    .then(response => response.json())
+    .then(data => console.log(data));
 }
