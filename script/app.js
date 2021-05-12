fbcc009d1c77a1af8d7336096f697699b515512c
AndrejaZi <andrejaziv1997@gmail.com>
2021-05-12 22:58:16 +0200

    post

--- script/app.js
+++ script/app.js
@@ -1,6 +1,14 @@
 
 async function myFunc(){
-    fetch('https://api.mocki.io/v1/ddb6d39b', { method: 'POST', mode : "no-cors" })
+    const options = {
+        method: 'POST',
+        mode: "no-cors",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    //https://ptsv2.com/t/andreja/post
+    fetch('https://ptsv2.com/t/andreja/post', options)
     .then(response => response.json())
     .then(data => console.log(data));
 }
